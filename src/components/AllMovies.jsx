3bdf888d7c3f1bff949d6c57d1046a9e37a8f3d4
okorojames <okorojameschizaram@gmail.com>
2023-04-11 19:08:54 +0100

    still working on the all movies section

--- src/components/AllMovies.jsx
+++ src/components/AllMovies.jsx
@@ -4,16 +4,24 @@ import vidIntro from "../video/vid-intro.mp4";
 const AllMovies = () => {
   return (
     <div className="all--movies--section" style={{ marginTop: "80px" }}>
-      <div className="all--movies--first--content">
-        <video
-          src={vidIntro}
-          autoPlay
-          muted
-          loop
-          className="all--movie--video"
-          controlsList="nodownload"
-          style={{ pointerEvents: "none" }}
-        ></video>
+      <div
+        className="all--movies--first--content"
+        style={{ marginBottom: "60px" }}
+      >
+        <div className="all--header--videos">
+          <video
+            src={vidIntro}
+            autoPlay
+            muted
+            loop
+            className="all--movie--video"
+            controlsList="nodownload"
+            style={{ pointerEvents: "none" }}
+          ></video>
+          <div className="video--overlay">
+            <h2 className="video--overlay--h2">All our available movies</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
